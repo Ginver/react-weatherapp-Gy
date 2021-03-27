4c98b4eec36e390127f4a3229ad4f5a93680d863
Ginver <74971813+Ginver@users.noreply.github.com>
2021-03-27 19:34:13 +0100

    api request & useState

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import axios from 'axios';
-
 import SearchBar from './components/searchBar/SearchBar';
 import TabBarMenu from './components/tabBarMenu/TabBarMenu';
 import MetricSlider from './components/metricSlider/MetricSlider';
+import { useState } from 'react';
 import './App.css';
 
 const apiKey = 'e1c2977a9608225f95553d9bfdfc239d';
 
 function App() {
-  async function fetchData() {
+  const [weatherData, setWeatherData] = useState(null);
+
+  const fetchData = async () =>  {
     try {
       const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=utrecht,nl&appid=${apiKey}&lang=nl`);
-      console.log(result.data);
+      setWeatherData(result.data);
+      // console.log(result.data) to see where to find the info in the object
     } catch (e) {
       console.error(e);
     }
@@ -26,9 +29,17 @@ function App() {
           <SearchBar/>
 
           <span className="location-details">
-            <h2>Bewolkt</h2>
-            <h3> </h3>
-            <h1>14 &deg;</h1>
+            {/*<h2>Bewolkt</h2>*/}
+            {/*<h3> </h3>*/}
+            {/*<h1>14 &deg;</h1>*/}
+            {/*place a condition to render data when the elements the data actually need it*/}
+            {weatherData &&
+            <>
+              <h2>{weatherData.weather[0].description}</h2>
+              <h3>{weatherData.name}</h3>
+              <h1>{weatherData.main.temp}</h1>
+            </> //* place extra fragments <> </> bc there are 3 elements en you can only retrun ONE element a time
+            }
 
             <button
                 type="button"
