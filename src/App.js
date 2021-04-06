5547c26be76ba6a662bb33dc3803f145d5f43034
Ginver <74971813+Ginver@users.noreply.github.com>
2021-04-06 15:47:35 +0200

    error message added

--- src/App.js
+++ src/App.js
@@ -1,74 +1,74 @@
-import React, { useState, useEffect  } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import SearchBar from './components/searchBar/SearchBar';
 import TabBarMenu from './components/tabBarMenu/TabBarMenu';
 import MetricSlider from './components/metricSlider/MetricSlider';
+import ForecastTab from './pages/forecastTab/ForecastTab';
 import './App.css';
 
-const apiKey = 'e1c2977a9608225f95553d9bfdfc239d';
+// LET OP: VOEG HIER JOUW API KEY IN
+const apiKey = '050ff8be31f74868842b18a0e5465d77';
 
 function App() {
   const [weatherData, setWeatherData] = useState(null);
   const [location, setLocation] = useState('');
+  const [error, setError] = useState(false);
 
   useEffect(() => {
-    // 1. we definieren de functie
-  const fetchData = async () =>  {
-    try {
-      const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location},nl&appid=${apiKey}&lang=nl`);
-      setWeatherData(result.data);
-      // console.log(result.data) to see where to find the info in the object
-    } catch (e) {
-      console.error(e);
+    async function fetchData() {
+      setError(false);
+      try {
+        const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location},nl&appid=${apiKey}&lang=nl`);
+        setWeatherData(result.data);
+        console.log(result.data)
+      } catch (e) {
+        console.error(e);
+        setError(true);
+      }
     }
-  }
-    // 2. we roepen de functie aan als location is veranderd, maar niet null is
+
     if (location) {
       fetchData();
     }
+
   }, [location]);
 
   return (
-    <>
-      <div className="weather-container">
+      <>
+        <div className="weather-container">
 
-        {/*HEADER -------------------- */}
-        <div className="weather-header">
-          <SearchBar setLocationHandler={setLocation}/>
+          {/*HEADER -------------------- */}
+          <div className="weather-header">
+            <SearchBar setLocationHandler={setLocation} />
 
+            {error && (
+                <span className="wrong-location-error">
+              Oeps! Deze locatie bestaat niet
+            </span>
+            )}
           <span className="location-details">
-            {/*<h2>Bewolkt</h2>*/}
-            {/*<h3> </h3>*/}
-            {/*<h1>14 &deg;</h1>*/}
-            {/*place a condition to render data when the elements the data actually need it*/}
             {weatherData &&
             <>
               <h2>{weatherData.weather[0].description}</h2>
               <h3>{weatherData.name}</h3>
               <h1>{weatherData.main.temp}</h1>
-            </> //* place extra fragments <> </> bc there are 3 elements en you can only retrun ONE element a time
+            </>
             }
-
-            {/*<button*/}
-            {/*    type="button"*/}
-            {/*    onClick={fetchData}>*/}
-            {/*  Haal data op!*/}
-            {/*</button>*/}
           </span>
-        </div>
-
-        {/*CONTENT ------------------ */}
-        <div className="weather-content">
-          <TabBarMenu/>
-
-          <div className="tab-wrapper">
-            Alle inhoud van de tabbladen komt hier!
           </div>
-        </div>
 
-        <MetricSlider/>
-      </div>
-    </>
+          {/*CONTENT ------------------ */}
+          <div className="weather-content">
+            <TabBarMenu/>
+
+            <div className="tab-wrapper">
+              <ForecastTab coordinates={weatherData && weatherData.coord}/>
+            </div>
+          </div>
+
+          <MetricSlider/>
+        </div>
+      </>
   );
 }
 
--- src/pages/forecastTab/ForecastTab.js
+++ src/pages/forecastTab/ForecastTab.js
@@ -1,85 +1,77 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './ForecastTab.css';
+import axios from "axios";
 
-function ForecastTab() {
-  return (
-    <div className="tab-wrapper">
-      <article className="forecast-day">
-        <p className="day-description">
-          Maandag
-        </p>
 
-        <section className="forecast-weather">
-            <span>
-              12&deg; C
-            </span>
-          <span className="weather-description">
-              Licht Bewolkt
-            </span>
-        </section>
-      </article>
+// LET OP: VOEG HIER JOUW API KEY IN
+const apiKey = '050ff8be31f74868842b18a0e5465d77';
 
-      <article className="forecast-day">
-        <p className="day-description">
-          Maandag
-        </p>
+function ForecastTab({ coordinates }) {
+    const [forecasts, setForecasts] = useState(null);
+    const [error, setError] = useState(false);
 
-        <section className="forecast-weather">
-            <span>
-              12&deg; C
-            </span>
-          <span className="weather-description">
-              Licht Bewolkt
-            </span>
-        </section>
-      </article>
+    function createDateString(timestamp) {
+        const day = new Date(timestamp * 1000);
+        return day.toLocaleDateString('nl-NL', { weekday: 'long' });
+    }
 
-      <article className="forecast-day">
-        <p className="day-description">
-          Maandag
-        </p>
+    useEffect(() => {
+        async function fetchData() {
+            setError(false);
 
-        <section className="forecast-weather">
-            <span>
-              12&deg; C
-            </span>
-          <span className="weather-description">
-              Licht Bewolkt
-            </span>
-        </section>
-      </article>
+            try {
+                const result = await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates?.lat}&lon=${coordinates?.lon}&exclude=minutely,current,hourly&appid=${apiKey}&lang=nl`);
+                setForecasts(result.data.daily.slice(1, 6));
+            } catch (e) {
+                console.error(e);
+                setError(true);
+            }
+        }
 
-      <article className="forecast-day">
-        <p className="day-description">
-          Maandag
-        </p>
+        if (coordinates) {
+            fetchData();
+        }
 
-        <section className="forecast-weather">
-            <span>
-              12&deg; C
-            </span>
-          <span className="weather-description">
-              Licht Bewolkt
-            </span>
-        </section>
-      </article>
+    }, [coordinates]);
 
-      <article className="forecast-day">
-        <p className="day-description">
-          Maandag
-        </p>
+    return (
+        <div className="tab-wrapper">
 
-        <section className="forecast-weather">
-            <span>
-              12&deg; C
-            </span>
-          <span className="weather-description">
-              Licht Bewolkt
-            </span>
-        </section>
-      </article>
-    </div>
-  );
-};
+
+            {forecasts && forecasts.map((forecast) => {
+                return (
+                    <article className="forecast-day" key={forecast.dt}>
+                        <p className="day-description">
+                            {createDateString(forecast.dt)}
+                        </p>
+
+                        <section className="forecast-weather">
+                            <span>{forecast.temp.day}
+                            </span>
+                            <span className="weather-description">
+                            {forecast.weather[0].description}
+                            </span>
+                        </section>
+                    </article>
+                )}
+            )}
+
+            {!forecasts && !error && (
+                <span className="no-forecast">
+                    Zoek eerst een locatie om het weer voor deze week te bekijken
+                </span>
+            )}
+
+            {error && (
+                <span>
+                    Er is iets misgegaan met het ophalen van de data.
+                </span>
+            )}
+
+
+
+        </div>
+    );
+}
 
 export default ForecastTab;
